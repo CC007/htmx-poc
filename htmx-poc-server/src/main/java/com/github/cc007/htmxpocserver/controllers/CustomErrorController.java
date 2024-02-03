5c69e7e88d661426d043d6c9c421a829a71f8b66
CC007 <Coolcat_the_best2@Hotmail.com>
2024-02-03 01:29:44 +0100

    Fix the menu for the error page

--- htmx-poc-server/src/main/java/com/github/cc007/htmxpocserver/controllers/CustomErrorController.java
+++ htmx-poc-server/src/main/java/com/github/cc007/htmxpocserver/controllers/CustomErrorController.java
@@ -1,19 +1,26 @@
 package com.github.cc007.htmxpocserver.controllers;
 
+import com.github.cc007.htmxpocserver.services.MenuItemService;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
+@RequiredArgsConstructor
 public class CustomErrorController implements ErrorController {
 
+    private final MenuItemService menuItemService;
+
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        model.addAttribute("menuItems", menuItemService.getMenuItems(null));
 
         return Optional.ofNullable(status)
                 .map(Object::toString)
